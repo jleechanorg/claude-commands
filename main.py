ec4dbc13706d3f9a7816e8d37fbe211c7a49b656
Jeff <jleechan@gmail.com>
2025-06-15 22:18:01 +0000

    Add initial Flask web application files

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    utc_time = datetime.now(pytz.utc)
+    pacific_time = utc_time.astimezone(pytz.timezone('US/Pacific'))
+    template_data = {
+        'server_time': pacific_time.strftime('%Y-%m-%d %H:%M:%S %Z'),
+        'page_title': 'World Architecture AI'
+    }
+    return render_template('index.html', **template_data)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
